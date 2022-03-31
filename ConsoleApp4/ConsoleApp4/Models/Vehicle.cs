5510bc3cee9393cdb59fdc24964b6325e7517738
MamedovVasif <24Q8KZ1@code.edu.az>
2022-03-31 13:23:56 +0400

    31.03.2022

---
+++ ConsoleApp4/ConsoleApp4/Models/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4.Models
+{
+    
+
+   abstract class Vehicle
+    {
+        private int _DriveTime;
+        private int _DrivePath;
+        public int DriveTime { get { return _DriveTime; } set { if (value > 0) { DriveTime = value; } } }
+        public int DrivePath { get { return _DrivePath; } set { if (value > 0) { DrivePath = value; } } }
+   public void AvarageSpeed()
+        {
+            Console.WriteLine($"Gedilen yol:{DrivePath/DriveTime}km/saatdir");
+        }
+    }
+    interface IEngine
+    {
+        public int HorsePower { get; set; }
+        public int TankSize { get; set; }
+        public int CurrentOil { get; set; }
+        public int FuelType { get; set; }
+        public void LeftFuelAmmount()
+        {
+            
+        }
+    }
+    interface IIWheel
+    {
+        public int WheelThickness { get; set; }
+    }
+    interface IITransmisson
+    {
+        public int TransmissionKind { get; set; }
+    }
+    
+}
